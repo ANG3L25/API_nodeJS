a7b8a60579a9e39a6c33af83cb4fe030448b73c7
User <user@localhost.localdomain>
2018-08-07 14:09:38 -0500

    API_Nodejs.0.0.1

---
+++ Random_api/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.get('/', () => {
+  res.send('index page');
+});
+
+app.get('/random/:numeroInicial/:numeroFinal', (req,res) => {
+  const min = parseInt(req.params.numeroInicial);
+  const max = parseInt(req.params.numeroFinal);
+
+  if(isNaN(min) || isNaN(max)){
+    res.status(404);
+    res.json({"error": 'Bad request'});
+    return;
+  }
+
+
+  const result = Math.floor(Math.random() * (max-min) + min);
+
+  res.json({"randomNumber":result});
+
+});
+
+app.listen(3000, () => {
+  console.log('server on port 3000');
+});
